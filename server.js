b89e4defb9e5b9a2b9e09ed0c3a43ceb77e73815
SiewLeng <siewleng.lim@gmail.com>
2021-10-09 18:17:10 +0800

    edit port

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Server is listening on port ${port}`);
